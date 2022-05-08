f304e74b700b55217d44cf21263b75939027e4e2
ola-kanakri <ola.kanakri12@gmail.com>
2022-05-08 03:57:07 +0300

    Done

--- addProduct.php
+++ addProduct.php
@@ -1,4 +1,4 @@
-<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <meta charset="utf-8">
@@ -17,18 +17,18 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
-    <title>Home</title>
+    <title>Add product</title>
 </head>
 
 <body >
    
-  
+<?php session_start(); ?>
       
     <!-- Navbar --> 
     <header >
-      <div class="menu-bar" style="position: fixed;">
+      <div class="menu-bar">
         <nav class="navbar navbar-expand-lg navbar-light "  >
-          <a class="navbar-brand" href="viewProduct.php" style="font-weight: bold; color: white;">Ola Kanakri</a>
+          <a class="navbar-brand" href="viewProduct.php" style="font-weight: bold; color: white;">OKstore</a>
 
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon" ></span>
@@ -39,8 +39,12 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto" >
 
-              <li class="nav-item ">
-                <a class="nav-link" href="viewProduct.php">Home </a>
+              <li class="nav-item " >
+                <a class="nav-link" href="viewProduct.php" >View Products </a>
+              </li>
+
+              <li class="nav-item " >
+                <a class="nav-link" href="addProduct.php" >Add Products </a>
               </li>
               
             </ul>
@@ -50,87 +54,133 @@
     </header>
 
 
-
+    <br><br>
     <div class="container">
-  	<div class="p-5 my-4 rounded-3" ></div>   
+  	<div class="my-4" style="font-weight:bold; text-align:center; font-size:30px;">Add Products</div>   
    
-<div class="container"  style = "background-color:#eeeee4;  padding : 20px 50px; " >
+    <div class="container"  style = "background-color:#e7e5f9;  padding : 20px 50px; " >
     <br><br>
 
 
 <!-- Form start here -->
-  <form  action="" method="post" style=" color: #d38312;" > 
 
-    <div class="form-group">
-      <label for="Item">Product Name :</label>
-        <input type="text" class="form-control" id="Item" placeholder="Enter Product Name" name="Item" required>
-    </div><br>
+<form action="addProduct.php" method="post">
+  <div class="form-group">
+    <label for="productName">Product name</label>
+    <input type="text" class="form-control" id="productName" placeholder="Enter product name" name="productName" Required>
+  </div><br>
 
-    <div class="form-group">
-      <label  for="Price">Price :</label>
-        <input type="number" class="form-control" id="Price" placeholder="Enter Price" name="Price" required>
-    </div><br>
+  <div class="form-group">
+    <label for="regularPrice">Regular price <span>&#36;</span></label>
+    <input type="number" class="form-control" id="regularPrice" placeholder="Regular price ($)" name="regularPrice" Required>
+  </div><br>
 
-    <div class="form-group">
-      <label for="Image">Image :</label>
-        <input type="text" class="form-control"  id="Image"  name="Image" required>
-    </div><br>
+  <div class="form-group">
+    <label for="salePrice">Sale price <span>&#36;</span></label>
+    <input type="number" class="form-control" id="salePrice" placeholder="Sale price ($)" name="salePrice" >
+  </div><br>
 
-    <div class="form-group">
-      <label  for="Describtion">Describtion :</label>
-     <textarea  class="form-control" id="Describtion" placeholder="Enter Describtion" name="Describtion" required ></textarea>      
-    </div><br>
+  <div class="form-group">
+    <label for="productImg">Product Image</label>
+    <input type="text" class="form-control" id="productImg" placeholder="Images/T-shirt.png" name="productImg" Required>
+  </div><br>
 
-    <div class="form-group">        
-      <input type="submit" class="btn"  name="addProduct_btn" value="Add Product" style="background-color: #d38312;  color: white; margin-left: 450px;">
-    </div>
+  <div class="form-group">
+    <label for="productDescription">Product short description</label>
+    <textarea class="form-control" id="productDescription" rows="3"  placeholder="Add product description" name="productDescription" Required></textarea>
+  </div><br>
 
-  </form>
-</div>
-</div>
+  <button type="submit" class="btn btn-primary" name="addButton" style="background-color:#1c9492;">Add Product</button>
+  <br><br>
+</form>
+</div> </div><br><br>
 
+<!-- Table start here -->
+<section>
+        <div class="container">
+            <table class="table table-striped">
+              
+                <thead>
+                  <tr>
+                    <th scope="col">Product Name</th>
+                    <th scope="col">Regular Price</th>
+                    <th scope="col">Sale Price</th>
+                    <th scope="col">Product Description</th>
+                  </tr>
+                  </thead>
 
+                  <tbody>
+                    <?php
 
-<div class="container"  style="margin:100px 0px; ">
-<div class="p-5 my-4  rounded-3" style ="text-align:center;">
-         
-         <h1  style=" color :#f4b7b4 ;font:bold;"> All Products </h1>
-         
-     </div>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Item Name</th>
-      <th scope="col">Price</th>
-      <th scope="col">Image</th>
-      <th scope="col">Describtion</th>
-    </tr>
-  </thead>
+                      
 
-  <tbody>
-      <?php 
+                      if(isset($_POST['addButton']))
+                      {
+                        // Add multiple products name separated by new line space in productName variable in the SESSION's array.
+                        $_SESSION['productName'] .= $_POST['productName']."<br>";
 
-      if(isset($_POST['addProduct_btn'])) 
-      { 
-          $_SESSION ['Item'] .= $_POST['Item']."<br>";
-          $_SESSION ['Price'] .= $_POST['Price']."<br>";
-          $_SESSION ['Image'] .= $_POST['Image']."<br>";
-          $_SESSION ['Describtion'] .= $_POST['Describtion']."<br>";
-          
-          echo " <tr scope='col'> <td>".$_SESSION ['Item'] 
-          ."</td> <td> ".$_SESSION ['Price']."</td> <td>". 
-          $_SESSION ['Image'] ."</td> <td> ".
-          $_SESSION ['Describtion'] ."</td> </tr> ";
-      }
-      ?>
-  </tbody>
-</table> 
+                        $_SESSION['regularPrice'] .= $_POST['regularPrice']."<br>";
+
+                        $_SESSION['salePrice'] .= $_POST['salePrice']."<br>";
+
+                        $_SESSION['productImg'] .= $_POST['productImg']."<br>";
+
+                        $_SESSION['productDescription'] .= $_POST['productDescription']."<br>";
 
 
+                        echo " <tr scope='col'> 
+                        <td>".$_SESSION ['productName']."</td> 
+                        <td> ".$_SESSION ['regularPrice']."</td> 
+                        <td>".$_SESSION ['salePrice'] ."</td> 
+                        <td> ".$_SESSION ['productDescription'] ."</td> 
+                        </tr> ";
 
-</div>
-</div>
+                      }
+                    
+                    ?>
+                  </tbody>
+            </table>
+        </div>
+    </section>
 
+    
+  <!-- Footer -->
+<footer class="new_footer_area bg_color">
+            <div class="new_footer_top">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-3 col-md-6">
+                            <div class="f_widget social-widget pl_70 wow fadeInLeft" data-wow-delay="0.8s" style="visibility: visible; animation-delay: 0.8s; animation-name: fadeInLeft;">
+                                <h2 class="f-title f_600 t_color f_size_18">Contact us</h2>
+                                <div class="f_social_icon">
+                                    <a href="#" class="fab fa-facebook"></a>
+                                    <a href="#" class="fab fa-twitter"></a>
+                                    <a href="#" class="fab fa-linkedin"></a>
+                                    <a href="#" class="fab fa-pinterest"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer_bg">
+                    <div class="footer_bg_one"></div>
+                    <div class="footer_bg_two"></div>
+                </div>
+            </div>
+            <div class="footer_bottom">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 col-sm-7">
+                            <p class="mb-0 f_400">© OlaKanakri Inc.. 2022 All rights reserved.</p>
+                        </div>
+                        <div class="col-lg-6 col-sm-5 text-right">
+                            <p>Made with <i class="icon_heart"></i> in <a href="#">OlaKanakri</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
 
 
     <!-- Optional JavaScript -->
--- viewProduct.php
+++ viewProduct.php
@@ -1,83 +1,148 @@
-<?php
 
-class ToDo {
-	
-	private $_dataArray,
-			$_dataArrayJson,
-			$_dataName;
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- Fontawesome CDN Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
+    <title>View product</title>
+</head>
 
-	public function __construct($dataName = false) {
-		if ($dataName) {
-			$this->_dataName = 'saves/' . $dataName;
-		} else {
-			$this->_dataName = "saves/data.txt";
-		}
-	}
+<body >
+   
+<?php session_start(); ?>
+      
+    <!-- Navbar --> 
+    <header >
+      <div class="menu-bar">
+        <nav class="navbar navbar-expand-lg navbar-light "  >
+          <a class="navbar-brand" href="viewProduct.php" style="font-weight: bold; color: white;">OKstore</a>
 
-	private function getToDoArray() {
-		$handel = fopen($this->_dataName, "r");
-		$this->_dataArrayJson = fread($handel, filesize ($this->_dataName));
-		$this->_dataArray = json_decode($this->_dataArrayJson, true);
-		fclose($handel);
-	}
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" ></span>
+           
+          </button>
 
-	private function setToDoArray($create, $editToDo = "") {
-		if($create == "add") {
-			$this->_dataArrayJson = json_encode($this->_dataArray + $editToDo);
-		} else {
-			$this->_dataArrayJson = json_encode($this->_dataArray);
-		}
-		$handle = fopen ($this->_dataName, "w");
-		fwrite ($handle, $this->_dataArrayJson);
-		fclose ($handle);
-		self::getToDoArray();
-		header('Location: http://' . htmlspecialchars($_SERVER["HTTP_HOST"]));
-	}
 
-	public function setToDo($newToDo) {
-		if ($newToDo != "") {
-			self::getToDoArray();
-			$newToDo = array($newToDo => "true");
-			self::setToDoArray("add", $newToDo);
-		}
-	}
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto" >
 
-	public function delToDo($idToDo) {
-		if (is_numeric($idToDo)) {
-			self::getToDoArray();
-			$arrayKeys = array_keys($this->_dataArray);
-			unset($this->_dataArray[$arrayKeys[--$idToDo]]);
-			self::setToDoArray("remove");
-		}
-	}
+              <li class="nav-item " >
+                <a class="nav-link" href="viewProduct.php" >View Products </a>
+              </li>
 
-	public function changeToDo($idToDo) {
-		if (is_numeric($idToDo)) {
-			self::getToDoArray();
-			$arrayKeys = array_keys($this->_dataArray);
-			if ($this->_dataArray[$arrayKeys[--$idToDo]] == "true") {
-				$this->_dataArray[$arrayKeys[$idToDo]] = "false";
-			} else {
-				$this->_dataArray[$arrayKeys[$idToDo]] = "true";
-			}
-			self::setToDoArray("change");
-		}
-	}
+              <li class="nav-item " >
+                <a class="nav-link" href="addProduct.php" >Add Products </a>
+              </li>
+              
+            </ul>
+          </div>
+        </nav>
+      </div>     
+    </header>
 
-	public function getToDo() {
-		self::getToDoArray();
-		$arrayCount = count($this->_dataArray);
-		$arrayKeys = array_keys($this->_dataArray);
-		for ($i=0; $i < $arrayCount; $i++) { 
-			$newi = $i;
-			echo '<h4>';
-			if ($this->_dataArray[$arrayKeys[$i]] == "false") {
-				echo '<tr><th><s>' . $arrayKeys[$i] . '</s></th> <th><a href="?change=' . ++$newi . '">[X]</a></th>';
-			} else {
-				echo '<th>' . $arrayKeys[$i] . '</th> <th><a href="?change=' . ++$newi . '">[✓]</a></th>';
-			}
-			echo ' <th><a href="?delet=' . $newi . '">[Löschen]</a></h4></th></tr>';
-		}
-	}
-}
+
+    <br><br>
+<div class="container">
+    
+    <div class=" my-4" ></div>   
+   
+    <div class="container"   >
+    <br><br>
+
+    <?php
+    if(!empty( $_SESSION ['productName']))
+    {   
+        $proName = explode("<br>",$_SESSION ['productName']) ;
+        $proPrice = explode("<br>",$_SESSION ['regularPrice']) ;
+        $salePrice = explode("<br>",$_SESSION ['salePrice']) ;
+        $proImg = explode("<br>",$_SESSION ['productImg']) ;
+        $proDesc = explode("<br>",$_SESSION ['productDescription']) ;
+
+        for ( $i = 1 ; $i < count($proName) ; $i++)
+        {
+            echo"
+            <div class='col-md-4 mt-2'>
+            <div class='card' style='width: 30rem; background-color:#e7e5f9; '>
+
+            <img class='card-img-top' src='Images/T-shirt.png' alt='Card image cap'>
+   
+               <div class='card-body'>
+               <h3 class='card-title' style='font-weight:bold;''>". $proName[$i]."</h3>
+               <p class='card-text'>". $proDesc[$i]."</p>
+               <p class='card-text'style='font-weight:bold;'> <del>Price: ". $proPrice[$i]." </del></p>
+               <p class='card-text' style='color: red;font-weight:bold;'>Sale price: ". $salePrice[$i]."</p>
+               </div>
+               </div><br><br>
+               </div>";
+
+               
+   
+       }
+   
+
+
+    }
 ?>
+</div> 
+</div>
+    
+  <!-- Footer -->
+<footer class="new_footer_area bg_color">
+            <div class="new_footer_top">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-3 col-md-6">
+                            <div class="f_widget social-widget pl_70 wow fadeInLeft" data-wow-delay="0.8s" style="visibility: visible; animation-delay: 0.8s; animation-name: fadeInLeft;">
+                                <h2 class="f-title f_600 t_color f_size_18">Contact us</h2>
+                                <div class="f_social_icon">
+                                    <a href="#" class="fab fa-facebook"></a>
+                                    <a href="#" class="fab fa-twitter"></a>
+                                    <a href="#" class="fab fa-linkedin"></a>
+                                    <a href="#" class="fab fa-pinterest"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer_bg">
+                    <div class="footer_bg_one"></div>
+                    <div class="footer_bg_two"></div>
+                </div>
+            </div>
+            <div class="footer_bottom">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 col-sm-7">
+                            <p class="mb-0 f_400">© OlaKanakri Inc.. 2022 All rights reserved.</p>
+                        </div>
+                        <div class="col-lg-6 col-sm-5 text-right">
+                            <p>Made with <i class="icon_heart"></i> in <a href="#">OlaKanakri</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
